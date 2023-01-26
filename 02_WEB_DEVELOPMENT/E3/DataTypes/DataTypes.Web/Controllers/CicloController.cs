ef68fbbab2ba88a16a285b5c9b41c31dd6318570
Carlos Vazquez <carloseliamvazquez@gmail.com>
2023-01-26 16:20:02 -0500

    Agregando la parte de Javascript al ejercicio 3 del tema 2

---
+++ 02_WEB_DEVELOPMENT/E3/DataTypes/DataTypes.Web/Controllers/CicloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataTypes.Web.Controllers
+{
+    public class CicloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 02_WEB_DEVELOPMENT/E3/DataTypes/DataTypes.Web/wwwroot/Customer.js
@@ -0,0 +1,49 @@
+﻿
+function fecha() {
+    var fecha = moment(new Date).format("DD-MM-YYYY");
+    return fecha
+}
+
+
+let Customer = [
+    {
+        "nombreCliente": "Chano",
+        "apellidoCliente": "Velazquez",
+        "tipoMensualidad": "normal",
+        "fechaIngreso" : fecha()
+
+    },
+    {
+        "nombreCliente": "Juan",
+        "apellidoCliente": "Sanchez",
+        "tipoMensualidad": "estudiante",
+        "fechaIngreso": fecha()
+
+    },
+
+    {
+        "nombreCliente": "Diana",
+        "apellidoCliente": "Mendez",
+        "tipoMensualidad": "normal",
+        "fechaIngreso": fecha()
+
+    },
+
+    {
+        "nombreCliente": "David",
+        "apellidoCliente": "Pot",
+        "tipoMensualidad": "estudiante",
+        "fechaIngreso": fecha()
+
+    }
+
+]
+
+var lista = document.getElementById("lista");
+
+for (var i = 0; i < Customer.length; i++) {
+    lista.innerHTML = lista.innerHTML + "<li>" + "<div>" + Customer[i].nombreCliente + "</div>" + "<div>" + Customer[i].apellidoCliente + "</div>" + "<div>" + Customer[i].fechaIngreso + "</div>" + "<div>" + Customer[i].tipoMensualidad + "</div>" + "</i>"
+}
+
+
+
