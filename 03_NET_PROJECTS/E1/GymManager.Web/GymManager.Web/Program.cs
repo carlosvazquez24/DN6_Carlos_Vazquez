42b8e9b38428559e570f06ea8428334fe22d8ec1
Carlos Vazquez <carloseliamvazquez@gmail.com>
2023-02-10 11:10:51 -0500

    Subiendo ejercicio 1

---
+++ 03_NET_PROJECTS/E1/GymManager.Web/GymManager.Web/Program.cs
@@ -0,0 +1,21 @@
+using GymManager.ApplicationServices.Members;
+using GymManager.ApplicationServices.Navigation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Service that redirects the view with the controller
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddTransient<IMembersAppServices, MembersAppServices>();
+builder.Services.AddTransient<IMenuAppService, MenuAppService>();
+
+var app = builder.Build();
+
+// function that allow us to use static files
+app.UseStaticFiles();
+
+//app.MapGet("/File1", () => DateTime.Now.ToString());
+
+app.MapControllerRoute( "default",  "{controller=home}/{action=Index}/{id?}");
+app.MapControllerRoute("default", "{controller=Attendance}/{action=MemberIn}/{id?}");
+
+app.Run();
